6b15b70ef45b501b7c4ec9264f650871046a835a
faissah <faissah@jahia.com>
2019-06-10 13:00:50 -0400

    Configure Docker container for DX SAML valve

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -8,15 +8,15 @@ $config = array(
 
     'example-userpass' => array(
         'exampleauth:UserPass',
-        'user1:user1pass' => array(
+        'user1:password' => array(
             'uid' => array('1'),
             'eduPersonAffiliation' => array('group1'),
-            'email' => 'user1@example.com',
+            'email' => 'user1@jahia.com',
         ),
-        'user2:user2pass' => array(
+        'user2:password' => array(
             'uid' => array('2'),
             'eduPersonAffiliation' => array('group2'),
-            'email' => 'user2@example.com',
+            'email' => 'user2@jahia.com',
         ),
     ),
 
